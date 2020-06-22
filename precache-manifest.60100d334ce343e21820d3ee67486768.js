01a6e7e499eca2bb348b340e44b6a2c7ac170dc2
Vigyan <vigyan.kayastha@gmail.com>
2020-06-22 10:07:57 -0400

    Updates

---
+++ precache-manifest.60100d334ce343e21820d3ee67486768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074c3ae8a42d0faad242f9427a68ae55",
+    "url": "/react-deploying/index.html"
+  },
+  {
+    "revision": "23e92e2661a33e1dab29",
+    "url": "/react-deploying/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e188069d14669c7dc26",
+    "url": "/react-deploying/static/js/2.81d88616.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploying/static/js/2.81d88616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e92e2661a33e1dab29",
+    "url": "/react-deploying/static/js/main.aaf52b5b.chunk.js"
+  },
+  {
+    "revision": "864300970b1069b0fb7b",
+    "url": "/react-deploying/static/js/runtime-main.9167b5c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploying/static/media/logo.5d5d9eef.svg"
+  }
+]);
